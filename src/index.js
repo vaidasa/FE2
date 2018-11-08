622469b4d98bbbaab6a099c87a475664ee39e983
Martynas Girdvainis <martynas.girdvainis@nfq.lt>
2018-11-08 16:35:59 +0200

    Add lecture changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './assets/style.scss';
-import App from './components/App';
-
-render(<App />, document.querySelector('#app'));
+console.log('Hello world!');
