009b8121e423298c5a6233db2fce385739bb9df0
Vaidas A <vaidastr@gmail.com>
2018-11-13 20:47:54 +0200

    cleanup

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -14,19 +14,6 @@ export default class Genre extends React.Component {
 
   }
 
-  requestGenreMovies = () => {
-    axios
-      .get(endpoints.genreMovies(this.props.genre.id))
-      .then((res) => this.setGenreMovieList(res.data.results))
-      .catch((error) => console.log(error));
-  };
-
-  setGenreMovieList = (genreMovieList) => {
-    this.setState({
-      genreMovieList,
-    });
-  };
-
   updateGenreMovies = () => {
 
   this.props.onReceivedNewList(this.state.genreMovieList);
